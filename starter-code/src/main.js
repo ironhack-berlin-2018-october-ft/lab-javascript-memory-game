779f55f8d6c53346359ed9086ebb6fb3ee8a82d3
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-10-16 23:43:57 +0200

    Add finished memory game. Missing animation

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -1,25 +1,24 @@
 var imgs = [
-  'aquaman.jpg',
-  'batman.jpg',
-  'captain-america.jpg',
-  'fantastic-four.jpg',
-  'flash.jpg',
-  'green-arrow.jpg',
-  'green-lantern.jpg',
-  'ironman.jpg',
-  'spiderman.jpg',
-  'superman.jpg',
-  'the-avengers.jpg',
-  'thor.jpg'
+  "aquaman.jpg",
+  "batman.jpg",
+  "captain-america.jpg",
+  "fantastic-four.jpg",
+  "flash.jpg",
+  "green-arrow.jpg",
+  "green-lantern.jpg",
+  "ironman.jpg",
+  "spiderman.jpg",
+  "superman.jpg",
+  "the-avengers.jpg",
+  "thor.jpg"
 ];
 
 // memoryGame is a global variable and you can access it with your browser console
 var memoryGame;
 
-$(document).ready(function () {
+$(document).ready(function() {
   memoryGame = new MemoryGame(imgs);
-
-  memoryGame.render()
+  console.log(this.pickedCards);
+  memoryGame.shuffleCards();
+  memoryGame.render();
 });
-
-
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,38 +1,92 @@
 class MemoryGame {
   constructor(imgs) {
-    this.cards = []
+    this.pickedCards = []; // the 2 cards the user has picked
+    this.pairsClicked = 0; // the amount of tries the user had so far
+    this.pairsGuessed = 0; // the guessed pairs so far
+    this.cards = [];
+    this.indexTemp = 0;
     for (var i = 0; i < imgs.length; i++) {
       var card = {
         img: imgs[i],
         isVisible: false
-      }
-      this.cards.push(card, card)
+      };
+      this.cards.push(card);
     }
-    // TODO: continue
+    for (var i = 0; i < imgs.length; i++) {
+      var card = {
+        img: imgs[i],
+        isVisible: false
+      };
+      this.cards.push(card);
+    }
   }
 
-  shuffleCards() {}
+  shuffleCards() {
+    var sortedCards = this.cards;
+    var shuffeledCards = [];
+    var amoutOfCards = sortedCards.length;
+    for (let numberOfCards = 0; numberOfCards < amoutOfCards; numberOfCards++) {
+      var randomNumber = Math.floor(Math.random() * sortedCards.length);
+      shuffeledCards[numberOfCards] = sortedCards[randomNumber];
+      sortedCards.splice(randomNumber, 1);
+    }
+    this.cards = shuffeledCards;
+  }
 
-  checkIfPair(firstCard, secondCard) {}
+  checkIfPair(firstCard, secondCard) {
+    this.pairsClicked++;
+    if (firstCard.img === secondCard.img) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
 
-  isFinished() {}
+  isFinished() {
+    if (this.pairsGuessed === (this.cards.length / 2)) return true;
+    return false;
+  }
 
   render() {
-    var html = '';
+    //Default position
+    var html = "";
+    var index;
+    var index2;
+    var turn;
 
     for (var i = 0; i < this.cards.length; i++) {
-      var card = this.cards[i]
-
+      var card = this.cards[i];
       html += '<div class="card" data-index="' + i + '">';
-      if (card.isVisible)
-        html += '  <img src="img/' + card.img + '">';
-      html += '</div>';
+      if (card.isVisible) html += '  <img src="img/' + card.img + '">';
+      html += "</div>";
     }
+    $("#memory_board").html(html);
 
-    $('#memory_board').html(html);
-
-    $('.card').click(function () {
-      // TODO: write the code that should be triggered when the user click on a card
+    $(".card").click(e => {
+      console.log("Test");
+      index = e.target.getAttribute("data-index");
+      if (!this.cards[index].isVisible) {
+        if (this.pickedCards.length === 0) {
+          this.pickedCards.push(this.cards[index]);
+          this.cards[index].isVisible = true;
+          this.indexTemp = index;
+        } else {
+          this.pickedCards.push(this.cards[index]);
+          if (this.checkIfPair(this.pickedCards[0], this.pickedCards[1])) {
+            this.cards[index].isVisible = true;
+            this.pickedCards = [];
+          } else {
+            $("[data-index=" + index + "]").html(
+              '  <img src="img/' + this.cards[index].img + '">'
+            );
+            this.cards[index].isVisible = false;
+            this.cards[this.indexTemp].isVisible = false;
+            this.pickedCards = [];
+          }
+        }
+      }
+      this.render();
+      if (this.isFinished()) alert("You made it!")
     });
   }
 }
